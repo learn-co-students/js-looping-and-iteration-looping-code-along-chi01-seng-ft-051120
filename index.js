130f1062026e19ae01382f661a3759a4319880d1
jayastronomic <juliansmith94@hotmail.com>
2020-06-25 14:16:08 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, string) {
+    const arr = []
+    for(let i = 0; i < array.length; i++) {
+        arr.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return arr
+}
+
+function countDown(integer) {
+    let i = integer
+    while(i >= 0 ) {
+        console.log(i)
+        i -= 1
+    }
+}
