b28ce48421b933b82330e161de60ea55cea5e76f
DanHazard <daniel.love.jr@gmail.com>
2020-07-05 17:33:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, eventType) {
+  let messageArray = [];
+  let message = "";
+  for (let i = 0; i < names.length; i++) {
+    let message = (`Thank you, ${names[i]}, for the wonderful ${eventType} gift!`);
+    messageArray.push(message);
+  }
+  return messageArray;
+}
+
+function countDown(num){
+  let startValue = num;
+  while (startValue >= 0) {
+    console.log(startValue--);
+  }
+}
